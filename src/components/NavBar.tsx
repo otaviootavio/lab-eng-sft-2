88537593f01f2915412774a0359e1b6c08d79127
otaviootavio <otaviovacari@usp.br>
2024-04-02 23:08:59 -0300

    add landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,25 @@
+import Link from "next/link";
 import AuthShowcase from "./AuthShowcase";
 
 function NavBar() {
   return (
     <nav className="bg-gradient-to-r from-[#2e026d] via-[#3A3480] to-[#454093] p-4 shadow-lg">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <div className="flex items-center">
+        <div className="flex items-center gap-5">
           <span className="text-xl font-bold uppercase tracking-wide text-white">
-            Painel do treino
+            <Link href="/" legacyBehavior>
+              <a className="nav-link">Home</a>
+            </Link>
+          </span>
+          <span className="text-xl font-bold uppercase tracking-wide text-white">
+            <Link href="/training" legacyBehavior>
+              <a className="nav-link">Training</a>
+            </Link>
+          </span>
+          <span className="text-xl font-bold uppercase tracking-wide text-white">
+            <Link href="/agenda" legacyBehavior>
+              <a className="nav-link">Agenda</a>
+            </Link>
           </span>
         </div>
         <AuthShowcase />
